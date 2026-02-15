35e31b17934d28fe4262a488fd45705491edac67
Your Name <you@example.com>
2026-02-15 16:06:21 +0530

    sdc theme tailwind

---
+++ sdc/drupal_tailwind_theme/src/StarterKit.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\drupal_tailwind_theme;
+
+use Drupal\Core\Theme\StarterKitInterface;
+
+/**
+ * Interacts with theme generation.
+ */
+final class StarterKit implements StarterKitInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function postProcess(string $working_dir, string $machine_name, string $theme_name): void {
+    $readme_file = "$working_dir/README.md";
+    try {
+      file_put_contents($readme_file, "$theme_name theme, generated from drupal_tailwind_theme. Additional information on generating themes can be found in the [Starterkit documentation](https://www.drupal.org/docs/core-modules-and-themes/core-themes/starterkit-theme).");
+    }
+    catch (\Throwable) {
+    }
+  }
+
+}
